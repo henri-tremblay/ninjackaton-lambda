bec19970bf76d13b609df057b957c6fe68017e71
Henri Tremblay <henri.tremblay@gmail.com>
2023-04-14 13:16:32 -0400

    Solutions to the kata

---
+++ src/main/java/com/ninja_squad/training/lambda/solution/TPSolution.java
@@ -0,0 +1,150 @@
+package com.ninja_squad.training.lambda.solution;
+
+import com.ninja_squad.training.lambda.Tweet;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Le TP Lambda
+ * @author JB
+ */
+public class TPSolution {
+
+    /**
+     * Écrivez les dates des tweets sur la sortie standard
+     */
+    public static void step1() {
+        Tweet.TWEETS.forEach(t -> System.out.println(t.date()));
+    }
+
+    /**
+     * Faites la même chose, sans appeler date() ni System.out.println()
+     */
+    public static void step2() {
+        Tweet.TWEETS.stream().map(Tweet::date).forEach(System.out::println);
+    }
+
+    /**
+     * Extrayez une List<String> qui contient les senders des tweets
+     */
+    public static List<String> step3() {
+        return Tweet.TWEETS.stream()
+                .map(Tweet::sender)
+                .toList();
+    }
+
+    /**
+     * Extrayez une List<String> qui contient les senders des tweets, sans duplicata
+     */
+    public static List<String> step4() {
+        return Tweet.TWEETS.stream()
+                .map(Tweet::sender)
+                .distinct()
+                .toList();
+    }
+
+    /**
+     * Extrayez une List<String> qui contient les senders des tweets, sans duplicata, triés par ordre alphabétique
+     */
+    public static List<String> step5() {
+        return Tweet.TWEETS.stream()
+                .map(Tweet::sender)
+                .distinct()
+                .sorted()
+                .toList();
+    }
+
+    /**
+     * Extrayez une List<Tweet> qui contient les tweets contenant le hashtag #lambda
+     */
+    public static List<Tweet> step6() {
+        return Tweet.TWEETS.stream()
+                .filter(t -> t.getHashTags().contains("#lambda"))
+                .toList();
+    }
+
+    /**
+     * Extrayez une List<Tweet> qui contient les tweets contenant le hashtag #lambda, triés par sender puis par date
+     */
+    public static List<Tweet> step7() {
+        return Tweet.TWEETS.stream()
+                .filter(t -> t.getHashTags().contains("#lambda"))
+                .sorted(Comparator.comparing(Tweet::sender).thenComparing(Tweet::date))
+                .toList();
+    }
+
+    /**
+     * Extrayez un Set<String> qui contient l'ensemble des hash tags des tweets
+     */
+    public static Set<String> step8() {
+        return Tweet.TWEETS.stream()
+                .flatMap(t -> t.getHashTags().stream())
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Créez une Map<String, List<Tweet>> qui contient, pour chaque sender, les tweets envoyés par ce sender
+     */
+    public static Map<String, List<Tweet>> step9() {
+        return Tweet.TWEETS.stream()
+                .collect(Collectors.groupingBy(Tweet::sender));
+    }
+
+    /**
+     * Extrayez deux listes: les tweets qui contiennent le hash tag #lambda, et ceux qui ne les contiennent pas.
+     */
+    public static Map<Boolean, List<Tweet>> step10() {
+        return Tweet.TWEETS.stream()
+                .collect(Collectors.partitioningBy(t -> t.getHashTags().contains("#lambda")));
+    }
+
+    /**
+     * Calculez le total du nombre de caractères des textes des tweets.
+     * Indice:
+     *     Utilisez un {@link java.util.stream.IntStream}
+     */
+    public static int step11() {
+        return Tweet.TWEETS.stream()
+                .map(Tweet::text)
+                .mapToInt(String::length)
+                .sum();
+    }
+
+    /**
+     * Calculez la moyenne du nombre de caractères des textes des tweets.
+     * Indice:
+     *     Utilisez un {@link java.util.stream.IntStream}
+     */
+    public static int step12() {
+        return (int) Tweet.TWEETS.stream()
+                .map(Tweet::text)
+                .mapToInt(String::length)
+                .average().getAsDouble();
+    }
+
+    /**
+     * Même chose, mais en calculant manuellement la somme.
+     * Indice:
+     *     Utilisez stream.collect(..., ..., ...) ou stream.map(...).reduce(...)
+     */
+    public static int step13() {
+        return Tweet.TWEETS.stream()
+                .map(Tweet::text)
+                .mapToInt(String::length)
+                .reduce(0, Integer::sum) / Tweet.TWEETS.size();
+    }
+
+    /**
+     * Faites la même chose, mais de manière parallèle
+     */
+    public static int step14() {
+        return Tweet.TWEETS.parallelStream()
+                .map(Tweet::text)
+                .mapToInt(String::length)
+                .reduce(0, Integer::sum) / Tweet.TWEETS.size();
+    }
+}
